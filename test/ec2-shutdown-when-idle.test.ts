b6c6c14caed07d8322279d49230f97cac56860ad
Simon Tardell <simon@tardell.se>
2024-02-20 21:49:13 +0100

    Initial commit

---
+++ test/ec2-shutdown-when-idle.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2ShutdownWhenIdle from '../lib/ec2-shutdown-when-idle-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2-shutdown-when-idle-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2ShutdownWhenIdle.Ec2ShutdownWhenIdleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
